44a780d4884b69721c9ef21829a958e985604360
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-19 23:29:56 +0200

    Se crean todas las rutas hijas en los modulos de routing para auth y products. Se añade la etiqueta router-outlet al layout-page de auth y products

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 const routes: Routes = [
   // Add routes here
@@ -10,6 +13,11 @@ const routes: Routes = [
     component: LayoutPageComponent,
     children: [
       { path: 'new-product', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: ':id', component: ProductPageComponent },
+      { path: '**', redirectTo: 'list' },
     ]
 
   }
