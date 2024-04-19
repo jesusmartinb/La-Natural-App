1581bd3e567a864278ec7d94e154b48b2f0b1f01
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-19 23:08:19 +0200

    Se establecen las rutas principales (auth, products, 404, '' y **) con carga perezosa de los módulos auth y products. Se establecen rutas principales para los módulos auth y products, así como una ruta hija para products

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 const routes: Routes = [
   // Add routes here
+  {
+    path: '',
+    component: LayoutPageComponent
+  }
 ];
 
 @NgModule({
--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Add routes here
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'new-product', component: NewPageComponent },
+    ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
