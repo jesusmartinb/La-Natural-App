f6ef30fb043323854f285efc14a0ea40d5dbe9cf
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-20 16:19:00 +0200

    Se maqueta la lista de componentes, creando un componente hijo card

---
+++ src/app/products/components/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product.interface';
+
+@Component({
+  selector: 'products-product-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  public product!: Product;
+
+  ngOnInit(): void {
+      if (!this.product) {
+          throw new Error('Product property is required');
+      }
+  }
+
+}
