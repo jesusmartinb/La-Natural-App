53166723f83246a99d60fd8b476388dc26ac0392
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-20 12:51:26 +0200

    Se crean variables de entorno y se actualiza angular.json

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+
+export const environment = {
+  baseURL: 'https://jesus-martin-blanco.com/api'
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+// En desarrollo, la URL base es http://localhost:3000
+export const environment = {
+  baseURL: 'http://localhost:3000'
+};
