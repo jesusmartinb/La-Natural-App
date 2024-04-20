c6a038c302269e599b091dd4bea614c068f9e3b7
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-20 18:06:05 +0200

    En products.service se crea un método para la obtención de un producto por su id en base a la url. Se implementa la lógica del componente product-page.component.ts

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Product } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit{
+
+  public product?: Product;
+
+  constructor(
+    private productsService: ProductsService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.productsService.getProductById(id)),
+      ).subscribe(product => {
+        if(!product) return this.router.navigate(['/products/list']);
+
+        this.product = product;
+        console.log({product})
+        return;
+    });
+  }
 
 }
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Product } from '../interfaces/product.interface';
 import { environments } from '../../../environments/environments';
 
@@ -14,5 +14,13 @@ export class ProductsService {
   getProducts(): Observable<Product[]> {
 
     return this.httpClient.get<Product[]>(`${this.baseUrl}/products`);
+  }
+
+  getProductById(id: string): Observable<Product|undefined> {
+
+    return this.httpClient.get<Product>(`${this.baseUrl}/products/${id}`)
+      .pipe(
+        catchError(error => of(undefined) )
+      )
   }
 }
