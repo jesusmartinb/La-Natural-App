015d3b3aba2988ca26e400551198f5108a87fd93
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-21 12:32:50 +0200

    Se crean los métodos de servicio de products.service.ts para los end-points de POST (añadir), PATCH (actualizar), DELETE (eliminar)producto

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 1. Clonar el proyecto
 2. Ejecutar `npm install`
-3. Levantar backend `npm run backend`
-4. Ejecutar la app `npm start` o bien `ng serve -o`
+3. Levantar backend `npm run backend` en una terminal de comandos
+4. Ejecutar la app `npm start` o bien `ng serve -o` en otra terminal de comandos
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Product } from '../interfaces/product.interface';
 import { environments } from '../../../environments/environments';
 
@@ -26,5 +26,23 @@ export class ProductsService {
 
   getSuggestions(query: string): Observable<Product[]> {
     return this.httpClient.get<Product[]>(`${this.baseUrl}/products?q=${query}&_limit=6`);
+  }
+
+  addProduct(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(`${this.baseUrl}/products`, product);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    if (!product.id) throw new Error('Product id is required to update a product.');
+
+    return this.httpClient.patch<Product>(`${this.baseUrl}/products/${product.id}`, product);
+  }
+
+  deleteProductById(id: number): Observable<boolean> {
+    return this.httpClient.delete(`${this.baseUrl}/products/${id}`)
+      .pipe(
+        catchError((error) => of(false)),
+        map((resp) => true)
+      );
   }
 }
