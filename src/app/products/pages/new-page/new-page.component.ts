507e4b7a417011cb5fcead31f504ee5dac5c8855
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-21 13:01:12 +0200

    Se implementa ReactiveForm en el controlador new-page.component.ts para el formulario productForm

--- src/app/products/pages/new-page/new-page.component.ts
+++ src/app/products/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Presentacion } from '../../interfaces/product.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './new-page.component.css'
 })
 export class NewPageComponent {
+
+  public productForm = new FormGroup({
+    id: new FormControl<number>(0),
+    nombre: new FormControl<string>('', { nonNullable: true }),
+    texto: new FormControl<string>(''),
+    marca: new FormControl<string>(''),
+    presentacion: new FormControl<Presentacion>(Presentacion.The300Gr),
+    preciobaseeu: new FormControl<number>(0),
+    descuento: new FormControl<number>(0),
+    imagen: new FormControl<string>(''),
+    categoria: new FormControl<string[]>(['']),
+    subcategoria: new FormControl<string[]>(['']),
+    cantidad: new FormControl<number>(0),
+    alt_img: new FormControl<string>(''),
+  });
 
   public presentacion = [
     { id: '300gr.', desc: '300gr.'},
